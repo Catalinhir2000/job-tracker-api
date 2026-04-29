410bf6f140447e49c4283dc446f014bf5f472eda
Cătălin-Alexandru Hirean <catalin@Catalin-Alexandrus-MacBook-Air.local>
2026-04-29 22:25:17 +0300

    Add app factory, config and extensions

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from app.config import Config
+from app.extensions import db, migrate, ma
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    ma.init_app(app)
+
+    return app
